36223bc851d58691102cb8fd2f7bf607311a006a
Vikas Pawar <pawarvikas534@gmail.com>
2024-04-03 14:55:06 +0530

    Update productRoutes.js

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,11 +1,14 @@
-const mongoose = require('mongoose')
+
 const express = require('express');
 const productModel = require('../models/productModel');
 const router = express.Router();
 
 
 router.get('/', async (req, res) => {
-    try {
+
+
+
+      try {
       const products = await productModel.find();
       res.json(products);
     } catch (err) {
@@ -27,6 +30,25 @@ router.get('/:id',async (req, res) => {
       res.status(500).json({ message: 'Server Error' });
     }
   })
+router.get('/cat/:category' , async(req , res)=>{
 
+  const category = req.params.category; //  target string comes from query string
+ 
+if(category){
+    try {
+    const matchingProducts = await productModel.find({
+      $or: [
+        { url: { $regex: `.*${category}.*`, $options: 'i' } },
+        { summary: { $regex: `.*${category}.*`, $options: 'i' } },
+        
+      ]
+    });
+    res.json(matchingProducts);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error finding products');
+  }
+}
+})
 
 module.exports = router;
